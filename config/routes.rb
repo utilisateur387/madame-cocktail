d44d7ab869cb5bcf1289fbd8a801936a2d35d4a9
Miebi Iyeyemi <iyeyemim@gmail.com>
2021-02-26 12:59:49 +0100

    Change root

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'doses/new'
-  get 'doses/create'
-  get 'doses/delete'
-  get 'cocktails/index'
-  get 'cocktails/show'
-  get 'cocktails/new'
-  get 'cocktails/create'
+  # get 'doses/new'
+  # get 'doses/create'
+  # get 'doses/delete'
+  # get 'cocktails/index'
+  # get 'cocktails/show'
+  # get 'cocktails/new'
+  # get 'cocktails/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
